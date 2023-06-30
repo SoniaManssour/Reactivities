99fd562bc061503b00b68cbf0b146ef8774f0ebf
sonman028 <120724306+sonman028@users.noreply.github.com>
2023-06-30 12:19:21 +0200

    End of section 9

--- client-app/src/features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/features/activities/dashboard/ActivityDashboard.tsx
@@ -4,24 +4,25 @@ import { useStore } from "../../../app/stores/store";
 import ActivityList from './ActivityList';
 import { useEffect } from "react";
 import LoadingComponent from "../../../app/layout/LoadingComponent";
+import ActivityFilters from "./ActivityFilters";
 
 export default observer(function ActivityDashboard() {
     const { activityStore } = useStore();
-    const {loadActivities, activityRegistry} = activityStore;
+    const { loadActivities, activityRegistry } = activityStore;
 
     useEffect(() => {
-      if (activityRegistry.size <= 1) loadActivities();
+        if (activityRegistry.size <= 1) loadActivities();
     }, [loadActivities])
-  
+
     if (activityStore.loadingInitial) return <LoadingComponent content='Loading app...' />
-    
+
     return (
         <Grid>
             <Grid.Column width='10'>
                 <ActivityList />
             </Grid.Column>
             <Grid.Column width='6'>
-    <h2>Activity filters</h2>
+                <ActivityFilters />
             </Grid.Column>
         </Grid>
     )
