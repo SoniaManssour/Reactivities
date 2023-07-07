52097809c8462d06b39dd766b8e763ea85a243cf
sonman028 <120724306+sonman028@users.noreply.github.com>
2023-07-07 13:16:47 +0200

    End of section 11

--- client-app/src/app/models/activity.ts
+++ client-app/src/app/models/activity.ts
@@ -1,7 +1,7 @@
 export interface Activity { //prefix I nie jest konieczny
     id: string;
     title: string;
-    date: string;
+    date: Date | null;
     description: string;
     category: string;
     city: string;
